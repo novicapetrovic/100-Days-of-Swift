e993446a40879367e1099dcf7f46d160404bb76c
Novica Petrovic <41298012+novicapetrovic10@users.noreply.github.com>
2020-02-24 08:32:13 +0000

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Novica Petrovic on 24/02/2020.
+//  Copyright © 2020 Novica Petrovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
