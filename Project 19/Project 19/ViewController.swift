51f83fdb383e044a53c85cc3dbba9c53ccdbd4cd
Novica Petrovic <41298012+novicapetrovic10@users.noreply.github.com>
2020-03-15 18:55:00 +0000

    Adding project 19

---
+++ Project 19/Project 19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 19
+//
+//  Created by Novica Petrovic on 15/03/2020.
+//  Copyright © 2020 Novica Petrovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
